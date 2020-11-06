c67f7607600a07ade4258a6c23af7f11c2d94050
ashpil <shpilenok2001@gmail.com>
2020-11-06 12:35:43 -0500

    underscores begone

--- lib/camera.ml
+++ lib/camera.ml
@@ -3,18 +3,18 @@ type t =
    target: Vector.t;
    aspect_ratio : float; (* ratio of width to height *)
    vertical: Vector.t;
-   v_f_of_v : float} (* v_f_of_v = vertical field of view*)
+   vfov : float} (* vfov = vertical field of view*)
 
 (* Not actuall useful at the moment, just dummy function so we can test scenes *)
 let from_json json = 
   let v = Vector.origin in
-  { origin = v; target = v; aspect_ratio = 0.0; vertical = v; v_f_of_v = 0.0}
+  { origin = v; target = v; aspect_ratio = 0.0; vertical = v; vfov = 0.0}
 
 let generate_ray camera x y = 
   let w = Vector.unit_vector (Vector.(-) camera.origin camera.target) in
   let u = Vector.unit_vector (Vector.(cross_prod) camera.vertical w) in
   let v = Vector.unit_vector (Vector.(cross_prod) w u) in
-  let d = camera.v_f_of_v in 
+  let d = camera.vfov in 
   let x_comp = Vector.length (Vector.( * ) u x) in 
   let y_comp = Vector.length (Vector.( * ) v y) in 
   let z_comp =  Vector.length (Vector.( * ) w (-.d)) in
