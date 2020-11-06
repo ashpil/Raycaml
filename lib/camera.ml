ed6431c3d5714c29d061cb7f32b15b64d2f158a4
Naomi Koh <nck23@cornell.edu>
2020-11-06 17:31:38 -0500

    implement generate_ray
    Co-authored-by: Brian Chu <5briancchu@gmail.com>

--- lib/camera.ml
+++ lib/camera.ml
@@ -1,14 +1,22 @@
 type t = 
   {origin : Vector.t;
-  lower_left : Vector.t;
-  vertical: Vector.t;
-  horizontal : Vector.t;
-  u : Vector.t;
-  v : Vector.t;
-  w : Vector.t;
-  lens_radius : float}
+   target: Vector.t;
+   aspect_ratio : float; (* ratio of width to height *)
+   vertical: Vector.t;
+   v_f_of_v : float} (* v_f_of_v = vertical field of view*)
 
 (* Not actuall useful at the moment, just dummy function so we can test scenes *)
 let from_json json = 
   let v = Vector.origin in
-  { origin = v; lower_left = v; vertical = v; horizontal = v; u = v; v = v; w = v; lens_radius = 0. }
+  { origin = v; target = v; aspect_ratio = 0.0; vertical = v; v_f_of_v = 0.0}
+
+let generate_ray camera x y = 
+  let w = Vector.unit_vector (Vector.(-) camera.origin camera.target) in
+  let u = Vector.unit_vector (Vector.(cross_prod) camera.vertical w) in
+  let v = Vector.unit_vector (Vector.(cross_prod) w u) in
+  let d = camera.v_f_of_v in 
+  let x_comp = Vector.length (Vector.( * ) u x) in 
+  let y_comp = Vector.length (Vector.( * ) v y) in 
+  let z_comp =  Vector.length (Vector.( * ) w (-.d)) in
+  let direction = Vector.create x_comp y_comp z_comp in
+  Ray.create camera.origin direction 
--- lib/camera.mli
+++ lib/camera.mli
@@ -1,4 +1,8 @@
 (* A representation of a camera *)
 type t
 
+(* Parses data from json file *)
 val from_json : Yojson.Basic.t -> t
+
+(* Creates a ray given camera and direction *)
+val generate_ray : t -> float -> float -> Ray.t
