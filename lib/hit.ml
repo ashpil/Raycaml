90b62973365cd1cd0d8a179a4a91a7ec5efa38be
ashpil <shpilenok2001@gmail.com>
2020-11-08 19:11:49 -0500

    comments to hit

--- lib/hit.ml
+++ lib/hit.ml
@@ -1,9 +1,14 @@
-type t = 
-  {t : float;
-          point : Vector.t;
-          normal : Vector.t; 
-          material : Material.t}
+type t = {
+  t : float;
+  point : Vector.t;
+  normal : Vector.t; 
+  direction: Vector.t;
+  material : Material.t
+}
 
-let create t point normal material = {t; point; normal; material }
+let create t point normal direction material = {t; point; normal; direction; material }
 
 let distance {t; _ } = t  
+
+let dir {direction; _ } = direction
+
